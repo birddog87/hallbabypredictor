2a4890bf5d9e0a3823056a4ce7df40a2fd952258
Nick <blender7@gmail.com>
2025-05-03 17:56:43 -0400

    fix due date

--- app/page.tsx
+++ app/page.tsx
@@ -33,14 +33,14 @@ export default function Home() {
                 <svg className="w-6 h-6 text-pink-400" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
                   <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z"></path>
                 </svg>
-                <span className="font-medium">Due Date: August 16th, 2025</span>
+                <span className="font-medium">Due Date: August 19th, 2025</span>
               </div>
               
               <div className="flex items-center gap-3 text-gray-600">
                 <svg className="w-6 h-6 text-pink-400" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
                   <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M12 8v4l3 3m6-3a9 9 0 11-18 0 9 9 0 0118 0z"></path>
                 </svg>
-                <span className="font-medium">Countdown: 99 days</span>
+                <span className="font-medium">Countdown: 108 days</span>
               </div>
             </div>
             
